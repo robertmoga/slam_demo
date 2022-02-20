82b2817443b81359425f9afaf0d02c52f34b622c
robert <robertmoga16@gmail.com>
2022-02-20 20:52:20 +0200

    Added dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,26 @@
 import os
-import streamlist as st
+import json
+import tqdm
+import pickle
+import shutil
+import datetime
+import tempfile
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import streamlit as st
+import plotly.graph_objs as go
+from pyproj import Transformer
+from PIL import Image
+
 
 def display():
-	pass
-	
+	st.header('SLAM Demo')
+	st.image('data/demo_img.jpg')
+
+
 if __name__ == "__main__":
 	display()
 
